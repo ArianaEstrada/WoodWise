491b94ceb7680d6b0bdf3171944adb928bec09c7
Edson Jesús <rodriguezparralesedson@gmail.com>
2025-02-25 07:19:53 -0600

    Edson for change

---
+++ resources/views/acerca_nosotros.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de - WoodWise</title>
+</head>
+<body>
+    <h1>Acerca de WoodWise</h1>
+    <p>Información sobre el proyecto...</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/acerca-de', function () {
+    return view('acerca_nosotros');
+})->name('acerca');
+
+Route::get('/contactos', function () {
+    return view('contactos');
+})->name('contacto');
