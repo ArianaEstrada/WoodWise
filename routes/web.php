0ee4c7ad19680c48fa270e3a35d7f18a521c9dec
Jesus <l202207003@vbravo.tecnm.mx>
2025-02-26 08:02:04 -0600

    Acerca de

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/acerca-de', function () {
-    return view('acerca_nosotros');
-})->name('acerca');
+Route::get('/acerca-de', function () {return view('acerca_nosotros');})->name('acerca');
 
-Route::get('/contactos', function () {
-    return view('contactos');
-})->name('contacto');
+Route::get('/contactos', function () {return view('contactos');})->name('contacto');
